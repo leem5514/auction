f714840991afef967a879e47a0d2f6de10a62d32
leem5514 <ikaros0569@gmail.com>
2024-07-31 14:11:10 +0900

    ebey service start

---
+++ src/main/java/com/example/beyond/ebey/EbeyApplication.java
@@ -0,0 +1,13 @@
+package com.example.beyond.ebey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbeyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/beyond/ebey/EbeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beyond.ebey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
